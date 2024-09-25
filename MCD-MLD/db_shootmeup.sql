521e5a2cb913468fdb8e98cf3265c330b5d42aa3
Bastien <po77qxd@eduvaud.ch>
2024-09-25 11:22:55 +0200

    update mcd and db creation file

--- MCD-MLD/db_shootmeup.sql
+++ MCD-MLD/db_shootmeup.sql
@@ -13,7 +13,6 @@ CREATE TABLE t_joueur(
 CREATE TABLE t_ennemi(
    ennemi_id INT AUTO_INCREMENT,
    nbrVies INT,
-   minutage INT,
    tir BOOLEAN,
    sprite ,
    PRIMARY KEY(ennemi_id)
@@ -21,41 +20,43 @@ CREATE TABLE t_ennemi(
 
 CREATE TABLE t_obstacle(
    obstacle_id INT AUTO_INCREMENT,
-   taille VARCHAR(50),
-   position_x_y VARCHAR(50),
+   hauteur INT,
+   largeur INT,
+   position_x INT,
+   position_y INT,
    sprite ,
-   comportement VARCHAR(50),
    PRIMARY KEY(obstacle_id)
 );
 
 CREATE TABLE t_niveau(
    niveau_id INT AUTO_INCREMENT,
-   t_joueur_joueur_id INT NOT NULL,
+   joueur_id INT NOT NULL,
    PRIMARY KEY(niveau_id),
-   FOREIGN KEY(t_joueur_joueur_id) REFERENCES t_joueur(joueur_id)
+   FOREIGN KEY(joueur_id) REFERENCES t_joueur(joueur_id)
 );
 
 CREATE TABLE t_highscores(
    score_id INT AUTO_INCREMENT,
    score INT,
    nom_joueur VARCHAR(50),
-   t_niveau_niveau_id INT NOT NULL,
+   niveau_id INT NOT NULL,
    PRIMARY KEY(score_id),
-   FOREIGN KEY(t_niveau_niveau_id) REFERENCES t_niveau(niveau_id)
+   FOREIGN KEY(niveau_id) REFERENCES t_niveau(niveau_id)
 );
 
-CREATE TABLE avoir_ennemi(
-   t_niveau_niveau_id INT,
-   t_ennemi_ennemi_id INT,
-   PRIMARY KEY(t_niveau_niveau_id, t_ennemi_ennemi_id),
-   FOREIGN KEY(t_niveau_niveau_id) REFERENCES t_niveau(niveau_id),
-   FOREIGN KEY(t_ennemi_ennemi_id) REFERENCES t_ennemi(ennemi_id)
+CREATE TABLE t_avoir_ennemi(
+   niveau_id INT,
+   ennemi_id INT,
+   PRIMARY KEY(niveau_id, ennemi_id),
+   FOREIGN KEY(niveau_id) REFERENCES t_niveau(niveau_id),
+   FOREIGN KEY(ennemi_id) REFERENCES t_ennemi(ennemi_id)
 );
 
-CREATE TABLE avoir_obstacles(
-   t_niveau_niveau_id INT,
-   t_obstacle_obstacle_id INT,
-   PRIMARY KEY(t_niveau_niveau_id, t_obstacle_obstacle_id),
-   FOREIGN KEY(t_niveau_niveau_id) REFERENCES t_niveau(niveau_id),
-   FOREIGN KEY(t_obstacle_obstacle_id) REFERENCES t_obstacle(obstacle_id)
+CREATE TABLE t_avoir_obstacles(
+   niveau_id INT,
+   obstacle_id INT,
+   PRIMARY KEY(niveau_id, obstacle_id),
+   FOREIGN KEY(niveau_id) REFERENCES t_niveau(niveau_id),
+   FOREIGN KEY(obstacle_id) REFERENCES t_obstacle(obstacle_id)
 );
+
